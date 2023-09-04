b03f8dd67c8c988e00b8ddd92e1c3230e9863f53
ravisumadi <143873837+ravisumadi@users.noreply.github.com>
2023-09-04 21:29:26 +0200

    new post test

---
+++ _posts/2023-09-04-Why-Science.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title: Why Science?
+date: 2023-09-04
+categories: opinion
+---
---
+++ makePost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter the title of the file:"
+read title
+
+# Remove spaces and replace with hyphens
+formatted_title=$(echo "$title" | tr ' ' '-')
+
+# Get current date in YYYY-MM-DD format
+current_date=$(date +'%Y-%m-%d')
+
+# Construct the filename
+filename="$current_date-$formatted_title.md"
+
+# Specify the target directory
+target_directory="/Users/ravi/Documents/website/echolocation-local/_posts"
+
+# Create the .md file with the constructed filename
+touch "$target_directory/$filename"
+
+echo "File created: $filename"
